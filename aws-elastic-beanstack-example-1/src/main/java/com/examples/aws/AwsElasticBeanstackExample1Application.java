f4c9f24ceb3581a872d37957054829a6e370e9ad
madhu1salibendla <madhu.salibendla@gmail.com>
2019-11-24 09:41:33 +0000

    Initial commit.

---
+++ aws-elastic-beanstack-example-1/src/main/java/com/examples/aws/AwsElasticBeanstackExample1Application.java
@@ -0,0 +1,13 @@
+package com.examples.aws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsElasticBeanstackExample1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsElasticBeanstackExample1Application.class, args);
+	}
+
+}
---
+++ aws-elastic-beanstack-example-1/src/test/java/com/examples/aws/AwsElasticBeanstackExample1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.examples.aws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsElasticBeanstackExample1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
